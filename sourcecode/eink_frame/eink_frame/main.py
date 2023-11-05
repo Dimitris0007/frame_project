cf9d055e0c40675ec39b928848fc4998a418c239
Dimitri <raf55@telenet.be>
2023-11-05 15:59:21 +0100

    input function for easy testing

--- sourcecode/eink_frame/eink_frame/main.py
+++ sourcecode/eink_frame/eink_frame/main.py
@@ -7,8 +7,14 @@ if __name__ == "__main__":
     # Create an instance of the ImageProcessor class
     image_processor = ImageProcessor()
 
+    image_name = input("Enter an image name: ")
+    
+    base_file_path = r'F:\EPAPERPROJ\frame_proj\e-Paper\RaspberryPi\python\pic\\' 
+
+    full_file_path = base_file_path + image_name
+
     #open image
-    image = Image.open(r'F:\EPAPERPROJ\frame_proj\e-Paper\RaspberryPi\python\pic\dimi.JPEG')
+    image = Image.open(full_file_path)
 
     # Process the image using the ImageProcessor instance
     target_width = 600
